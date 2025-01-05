65a2a1906d33ab4f2a318bae253ff3796f3cb275
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-05 17:59:59 +0700

    connect database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestConnectionService } from './config/testConnection';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+require('dotenv').config();
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        isGlobal: true,
+      }
+    ),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TestConnectionService],
 })
-export class AppModule {}
+export class AppModule { }
