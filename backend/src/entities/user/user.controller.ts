1c153069eb3895334ee5ef70508367d1843b2be0
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-13 02:24:17 +0700

    update src

---
+++ backend/src/entities/user/user.controller.ts
@@ -0,0 +1,30 @@
+import { BadRequestException, Body, Controller, Get, HttpCode, Post, ValidationPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { ApiBearerAuth } from '@nestjs/swagger';
+
+@Controller('user')
+@ApiBearerAuth()
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Get('getall')
+    async getUsers() {
+        try {
+            return await this.userService.getUsers();
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+    }
+
+    @Post('create')
+    @HttpCode(201)
+    async createUser(@Body(new ValidationPipe()) user: User) {
+        try {
+            const createdUser = await this.userService.createUser(user);
+            return createdUser;
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+    }
+}
---
+++ backend/src/entities/user/user.entity.ts
@@ -0,0 +1,59 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsOptional, IsPhoneNumber, Length } from "class-validator";
+import { Role } from "src/model/enum";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ nullable: false, unique: true })
+    @IsEmail({}, { message: 'Invalid email' })
+    @ApiProperty()
+    email: string;
+
+    @Column({ nullable: false })
+    @Length(6, 25, { message: 'Password must be at least 6 characters and 20 characters' })
+    @ApiProperty()
+    password: string;
+
+    @Column()
+    @ApiProperty()
+    firstName: string;
+
+    @Column()
+    @ApiProperty()
+    lastName: string;
+
+    @Column()
+    @IsPhoneNumber('VN')
+    @ApiProperty()
+    phone: string;
+
+    @Column({ nullable: true })
+    @IsOptional()
+    @ApiProperty()
+    age: number
+
+    @Column({ nullable: true })
+    @IsOptional()
+    @ApiProperty()
+    address: string
+
+    @Column()
+    @ApiProperty()
+    role: Role
+
+    @Column()
+    @CreateDateColumn()
+    @ApiProperty()
+    createdAt: Date
+
+    @Column()
+    @UpdateDateColumn()
+    @ApiProperty()
+    updatedAt: Date
+
+}
