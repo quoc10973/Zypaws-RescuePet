f0e84e247d7a84b13a402da240bc767b4c5aa4a9
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-13 02:25:22 +0700

    add new enum

--- backend/src/model/enum.ts
+++ backend/src/model/enum.ts
@@ -2,3 +2,20 @@ export enum Role {
     ADMIN = 'ADMIN',
     USER = 'USER'
 }
+
+export enum Species {
+    DOG = 'DOG',
+    CAT = 'CAT',
+    BIRD = 'BIRD'
+}
+
+export enum Gender {
+    MALE = 'MALE',
+    FEMALE = 'FEMALE'
+}
+
+export enum Status {
+    AVAILABLE = 'AVAILABLE',
+    PENDING = 'PENDING',
+    ADOPTED = 'ADOPTED'
+}
