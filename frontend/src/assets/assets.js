02f3319cf2c72c1d854a04f309ee66e67c85fe46
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-23 02:42:34 +0700

    slogan image

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,9 @@
 import logo from './logo.png'
+import decorate1 from './decorate1.png'
+import decorate2 from './decorate2.png'
+
 export const assets = {
     logo,
+    decorate1,
+    decorate2
 }
