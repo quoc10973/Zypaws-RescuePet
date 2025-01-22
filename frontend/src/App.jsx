bde7422bdfb3d8a3d37f2edc209540fcf7655d41
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-23 02:42:52 +0700

    app layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import Footer from "./component/Footer"
-import Header from "./component/Header"
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Slogan from './component/Slogan';
 
-function App() {
+const App = () => {
   return (
     <>
+      <Slogan />
+      <Header />
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[10vw]">
-        <Header />
-        App
-        <Footer />
+        {/* Nơi hiển thị các trang con */}
+        <Outlet />
       </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
