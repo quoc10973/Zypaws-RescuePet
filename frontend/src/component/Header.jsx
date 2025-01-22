6603a8dba241407d407a4448cda0878d87d87159
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-23 02:44:19 +0700

    header

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className='bg-slate-900  flex items-center justify-between py-4 font-medium'>
-            <img src={assets.logo} alt='logo' className='w-36' />
+        <div className='bg-gray-200 flex items-center justify-center font-medium h-[40px]'>
+            {/* <img src={assets.logo} alt='logo' className='w-36' /> */}
+            <ul className='hidden sm:flex gap-5 space-x-9 font-poppins text-sm text-gray-700'>
+                <NavLink to='/home' className='flex flex-col items-center gap-1' >
+                    <p>Home</p>
+                </NavLink>
+                <NavLink to='/about' className='flex flex-col items-center gap-1' >
+                    <p>About</p>
+                </NavLink>
+                <NavLink to='/contact' className='flex flex-col items-center gap-1' >
+                    <p>Contact</p>
+                </NavLink>
+            </ul>
         </div>
     )
 }
