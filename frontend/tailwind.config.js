277ed4a79b211c1a6d61c3c3c7b361bbcd062057
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-24 00:57:21 +0700

    heartbeat animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,25 @@ export default {
         roboto: ['Roboto', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
       },
+      animation: {
+        heartbeat: "heartbeat 1.5s infinite", // define animation heartbeat
+      },
+      keyframes: {
+        heartbeat: {
+          "0%, 100%": {
+            transform: "scale(1)", // state begin and end
+          },
+          "25%": {
+            transform: "scale(1.1)", // expand at 25% time
+          },
+          "50%": {
+            transform: "scale(1.2)", // expand at 50% time
+          },
+          "75%": {
+            transform: "scale(1.1)", // expand at 75% time
+          },
+        },
+      },
     },
   },
   plugins: [],
