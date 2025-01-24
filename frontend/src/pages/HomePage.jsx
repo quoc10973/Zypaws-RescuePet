e082d45c7d2f05da46bd4c1e22725800092a268e
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-24 18:25:53 +0700

    homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Banner from '../component/Banner'
 
 const HomePage = () => {
     return (
-        <div>App</div>
+        <div >
+            <Banner />
+            <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[10vw]">
+                App
+            </div>
+        </div>
     )
 }
 
