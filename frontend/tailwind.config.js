6b9c954887ea3503c6130495593aa00a758c463a
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-24 18:25:03 +0700

    animation config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,9 +10,12 @@ export default {
         quicksand: ['Quicksand', 'sans-serif'],
         roboto: ['Roboto', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
+        dancing: ['Dancing Script', 'cursive'],
       },
       animation: {
         heartbeat: "heartbeat 1.5s infinite", // define animation heartbeat
+        'fade-in': 'fadeIn 1.5s ease-in forwards',
+        'fade-in-delayed': 'fadeIn 2s ease-in forwards',
       },
       keyframes: {
         heartbeat: {
@@ -28,6 +31,10 @@ export default {
           "75%": {
             transform: "scale(1.1)", // expand at 75% time
           },
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
         },
       },
     },
