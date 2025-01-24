e9d3c7698c46daf17d6b59d4dd29ee39c567f4b0
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-24 18:24:48 +0700

    add banner

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,11 @@
 import logo from './logo.png'
 import decorate1 from './decorate1.png'
 import decorate2 from './decorate2.png'
+import banner from './banner.jpg'
 
 export const assets = {
     logo,
     decorate1,
-    decorate2
+    decorate2,
+    banner,
 }
