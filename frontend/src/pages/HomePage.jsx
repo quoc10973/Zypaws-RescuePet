8761a8f124d3392a39496a9d7b8b3f5f16548e96
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-26 01:27:10 +0700

    update homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Banner from '../component/Banner';
+import { Link } from 'react-router-dom';
+import { assets } from '../assets/assets';
 
 const HomePage = () => {
     const [isVisible, setIsVisible] = useState(false);
@@ -9,7 +11,7 @@ const HomePage = () => {
     const handleScroll = () => {
         if (elementRef.current) {
             const rect = elementRef.current.getBoundingClientRect();
-            const isInViewport = rect.top <= window.innerHeight && rect.bottom >= 0;
+            const isInViewport = rect.top <= window.innerHeight + 500 && rect.bottom >= 0;
             setIsVisible(isInViewport);
         }
     };
@@ -31,8 +33,11 @@ const HomePage = () => {
         <div>
             <Banner />
             <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[10vw]">
+                <div className="flex justify-center items-center py-10">
+                    <img src={assets.decorate3} alt="Decorate 3" className="w-12 h-44 mb-[-50px]" />
+                </div>
                 <div className="text-center items-start space-y-4 p-12">
-                    <p className="font-playwrite font-bold text-green-700 text-1xl sm:text-2xl md:text-3xl lg:text-4xl">
+                    <p className="font-playwrite font-bold text-green-700 text-1xl sm:text-3xl md:text-3xl lg:text-4xl">
                         Rescue Stray Pet,
                     </p>
                     <p className="font-quicksand font-semibold text-gray-600 text-lg sm:text-xl md:text-2xl">
@@ -51,11 +56,30 @@ const HomePage = () => {
                         <br />
                         <br />
                         <b>Our work</b> has made a significant difference in the Australian rescue community, inspiring thousands of people to adopt and care for animals in need.
-                        But, until every pet is safe, respected, and loved, we all still have big, hairy work to do. Find out more about our mission to help save 100,000 healthy and rehomable pets each year.
+                        But, until every pet is safe, respected, and loved, we all still have big, hairy work to do. <Link to='/about-the-safe-sound-pets-program' className="hover:underline text-green-600"><b>Find out</b></Link>  more about our mission to help save 100,000 healthy and rehomable pets each year.
                     </p>
+                    <br />
+                    <div className="relative group">
+                        <Link
+                            to="about-the-safe-sound-pets-program"
+                            className="relative inline-block font-lora italic text-white font-semibold py-2 px-9 rounded-lg bg-slate-800 transition-transform duration-300 group-hover:-translate-y-1 group-hover:shadow-lg group-hover:shadow-gray-900/50"
+                        >
+                            <span className="absolute inset-0 w-full h-full  rounded-lg opacity-0 group-hover:opacity-100 group-hover:blur-md transition-opacity duration-300"></span>
+                            <span className="relative z-10">Find Out More</span>
+                        </Link>
+                    </div>
+                    <br />
+                    <p ref={elementRef} className="font-lora font-bold text-gray-600 text-lg sm:text-xl md:text-4xl">
+                        Many years of impact
+                    </p>
+                    <p ref={elementRef} className={`font-quicksand font-semibold text-gray-600 text-xs sm:text-xs md:text-sm transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+                        We've been saving lives and creating families for over 3 years.
+                    </p>
+
+
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
