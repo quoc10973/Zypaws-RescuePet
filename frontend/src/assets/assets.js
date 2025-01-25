10dc24e3ee0290c942482c54cfe8cdbb4c5a4572
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-26 01:26:41 +0700

    decorate3

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,7 @@
 import logo from './logo.png'
 import decorate1 from './decorate1.png'
 import decorate2 from './decorate2.png'
+import decorate3 from './decorate3.png'
 import banner1 from './banner1.jpg'
 import banner2 from './banner2.jpg'
 import banner3 from './banner3.jpg'
@@ -10,6 +11,7 @@ export const assets = {
     logo,
     decorate1,
     decorate2,
+    decorate3,
     banner1,
     banner2,
     banner3,
