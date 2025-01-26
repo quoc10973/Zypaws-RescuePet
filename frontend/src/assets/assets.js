849ed9699d8c0ad06731da24e89c8227a5b37db7
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-01-26 17:40:29 +0700

    add decorate

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,6 +2,8 @@ import logo from './logo.png'
 import decorate1 from './decorate1.png'
 import decorate2 from './decorate2.png'
 import decorate3 from './decorate3.png'
+import decorate4 from './decorate4.gif'
+import decorate5 from './decorate5.png'
 import banner1 from './banner1.jpg'
 import banner2 from './banner2.jpg'
 import banner3 from './banner3.jpg'
@@ -12,8 +14,10 @@ export const assets = {
     decorate1,
     decorate2,
     decorate3,
+    decorate4,
+    decorate5,
     banner1,
     banner2,
     banner3,
-    banner4
+    banner4,
 }
