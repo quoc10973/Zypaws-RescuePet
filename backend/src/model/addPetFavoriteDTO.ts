f996d0200605a42f78b272d5dfc2fa3750da7438
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-03-11 23:09:45 +0700

    delete

--- backend/src/model/addPetFavoriteDTO.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber } from "class-validator";
-
-export class AddPetFavoriteDTO {
-    @ApiProperty()
-    @IsNumber()
-    petId: number;
-}
