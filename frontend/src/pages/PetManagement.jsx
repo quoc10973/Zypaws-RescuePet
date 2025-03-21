9e3e1ace9ec6dc5defd4a2f0f8b3d678ac022cec
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-03-21 13:21:53 +0700

    use env varible

--- frontend/src/pages/PetManagement.jsx
+++ frontend/src/pages/PetManagement.jsx
@@ -86,14 +86,14 @@ const PetManagement = () => {
     const uploadImage = async (file) => {
         const formData = new FormData();
         formData.append("file", file);
-        formData.append("upload_preset", "zypaws_preset"); // Thay bằng upload_preset của bạn
-        formData.append("folder", "Pets");
+        formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET); // Thay bằng upload_preset của bạn
+        formData.append("folder", import.meta.env.VITE_CLOUDINARY_FOLDER);
 
         try {
-            const response = await fetch("https://api.cloudinary.com/v1_1/dl0dri4pf/image/upload", {
+            `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`, {
                 method: "POST",
                 body: formData
-            });
+            };
 
             const data = await response.json();
             return data.secure_url; // URL ảnh sau khi upload
