64d79d854c0c6e0914bfd6e586467ae75c2c4a0f
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-04-03 01:37:07 +0700

    fix redirect link

--- src/component/MobileTopBar.jsx
+++ src/component/MobileTopBar.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
 const MobileTopBar = () => {
     return (
         <div className="left-0 w-full bg-gray-100 text-gray-800 px-4 py-3 text-center shadow-md z-50 flex justify-between items-center sm:hidden">
             <p className="text-sm font-quicksand max-w-md mx-auto flex items-center gap-2">
-
                 <span>
-                    <a href="/about" className="text-purple-600 font-semibold hover:underline">
+                    <Link to="/about" className="text-purple-600 font-semibold hover:underline">
                         Learn more
-                    </a>{' '}
+                    </Link>{' '}
                     about how we make a difference.
                 </span>
             </p>
